b6902e84b9150d4bceb841a36fcde0c27a39ee1d
ifly7charlie <melissa-github@littlebluecar.co.uk>
2022-02-07 21:03:11 +0100

    Update aprs.js
    comments and general tidy

--- bin/aprs.js
+++ bin/aprs.js
@@ -38,17 +38,17 @@ let stations = {};
 let globalDb = undefined;
 let statusDb = undefined;
 
+// Least Recently Used cache for Station Database connectiosn 
 import LRU from 'lru-cache'
-
 const options = { max: process.env.MAX_STATION_DBS||800,
 				  length: function (n, key) { return 1 },
-				  dispose: function (key, n) { console.log( "flushed "+key+" from cache" ); },
+				  dispose: function (key, n) { n.close(); console.log( `closed database for ${key}` ); },
 				  updateAgeOnGet: true, stale: true,
 				  maxAge: (process.env.STATION_DB_EXPIRY_HOURS||4) * 3600 * 1000 }
 , stationDbCache = new LRU(options)
 
 
-//
+// track any setInterval calls so we can stop them when asked to exit
 let intervals = [];
 
 // APRS connection
@@ -67,16 +67,26 @@ import { PassThrough } from 'stream';
 import { makeTable, tableFromArrays, RecordBatchWriter, makeVector, FixedSizeBinary,
 		 Utf8, Uint8, makeBuilder } from 'apache-arrow';
 
+// How many stations we will report to the front end, list is sorted so this means
+// 20 busiest stations
 const numberOfStationsToTrack = 20;
 
+// Default paths, can be overloaded using .env.local
 let dbPath = './db/';
 let outputPath = './public/data/';
 
-//
+// We need to use a protected data structure to generate ids
+// for the station ID. This allows us to use atomics, will also
+// support clustering if we need it
 const sabbuffer = new SharedArrayBuffer(2);
 const nextStation = new Uint16Array(sabbuffer);
 
-// Set up background fetching of the competition
+// Run stuff magically
+main()
+    .then("exiting");
+
+//
+// Primary configuration loading and start the aprs receiver
 async function main() {
 
 	// Load the configuration from a file
@@ -96,44 +106,35 @@ async function main() {
 	// Open our databases
 	globalDb = LevelUP(LevelDOWN(dbPath+'global'))
 	statusDb = LevelUP(LevelDOWN(dbPath+'status'))
-	
+
+	// And start listening for messages
 	await startAprsListener();
 }
 
+//
+// Tidily exit if the user requests it
+// we need to stop receiving,
+// output the current data, close any databases,
+// and then kill of any timers
 async function handleExit(signal) {
 	console.log(`${signal}: flushing data`)
 	connection.exiting = true;
 	connection.disconnect();
+	await produceOutputFiles();
+	stationDbCache.forEach( async function (db,key) {
+		db.close();
+	});
+	globalDb.close();
+	statusDb.close();
 	for( const i of intervals ) {
 		clearInterval(i);
 	}
-	await produceOutputFiles();
 	console.log(`${signal}: done`)
 }
 process.on('SIGINT', handleExit);
 process.on('SIGQUIT', handleExit);
 process.on('SIGTERM', handleExit);
 
-main()
-    .then("exiting");
-
-//
-// Dump all of the output files that need to be dumped
-async function produceOutputFiles() {
-	console.log( `producing output files for ${stationDbCache.length} stations + global ` )
-	
-	// each of the stations
-	stationDbCache.forEach( async function (db,key) {
-		await produceOutputFile( key, db );
-	});
-		
-	// And the global output
-	await produceStationFile( statusDb );
-	await produceOutputFile( 'global', globalDb );
-
-	// Flush old database from the cache
-	stationDbCache.prune();
-}
 
 //
 // Connect to the APRS Server
@@ -314,36 +315,32 @@ async function processPacket( packet ) {
     const td = Math.floor(now - packet.timestamp);
 
     // Look it up, have we had packets for this before?
+	// we use this simply to make sure we aren't constantly recording points
+	// for stationary gliders
     let glider = gliders[flarmId];
-
-	// If it is undefined then we drop everything from here on
 	if( ! glider ) {
 		glider = gliders[flarmId] = { lastTime: packet.timestamp };
 	}
 
-    // Check to make sure they have moved or that it's been about 10 seconds since the last update
+    // Check to make sure they have moved more than 50 meters, thermal circle normally 100+ m
     // this reduces load from stationary gliders on the ground and allows us to track stationary gliders
     // better. the 1 ensures that first packet gets picked up after restart
     const distanceFromLast = glider.lastPoint ? distance( jPoint, glider.lastPoint ) : 1;
 	const elapsedTime = packet.timestamp - glider.lastTime;
 	
-    if( distanceFromLast < 0.04 && elapsedTime < 30 ) {
+    if( distanceFromLast < 0.05  ) {
 		glider.stationary++;
         return;
     }
-    if( distanceFromLast > 100 && distanceFromLast/(elapsedTime/3600) > 400 ) {
-		glider.lastMoved = packet.timestamp;
-		glider.jumps = (glider.jumps||0)+1;
-    }
-	
+
+	// If it has moved then save it away to make sure we aren't stationary
     if( packet.timestamp > glider.lastTime ) {        
 		glider.lastPoint = jPoint;
-		glider.lastAlt = altitude;
         glider.lastTime = packet.timestamp;
     }
 
+	// Look for signal strength and checksum - we will ignore any packet without these
 	const values = packet.comment.match( /([0-9.]+)dB ([0-9])e/ );
-
 	if( values ) {
 		const strength = Math.round(values[1]);
 		const crc = parseInt(values[2]);
@@ -351,7 +348,6 @@ async function processPacket( packet ) {
 		// If we have no signal strength then we'll ignore the packet... don't know where these
 		// come from or why they exist...
 		if( strength > 0 ) {
-//		packetCallback( sender, h3.geoToH3(packet.latitude, packet.longitude, 8), altitude, altitude, glider, crc, strength );
 
 			// Enrich with elevation and send to everybody, this is async
 			getElevationOffset( packet.latitude, packet.longitude,
@@ -384,6 +380,8 @@ async function packetCallback( station, h3id, altitude, agl, glider, crc, signal
 	mergeDataIntoDatabase( station, stationid, globalDb, h3.h3ToParent(h3id,7), altitude, agl, crc, signal);
 }
 
+//
+// Generate a fake structure that maps directly into the byte array we are working with
 const normalLength = 3*4+3*2+2;
 function mapping(buffer) {
 		const extraStationSlots = (buffer.byteLength - normalLength)/6;
@@ -404,6 +402,10 @@ function mapping(buffer) {
 		}
 }
 
+//
+// We store the database records as binary bytes - in the format described in the mapping() above
+// this reduces the amount of storage we need and means we aren't constantly parsing text
+// and printing text.
 async function mergeDataIntoDatabase( station, stationid, db, h3, altitude, agl, crc, signal ) {
 
 	function update(buffer, release) {
@@ -497,9 +499,36 @@ async function mergeDataIntoDatabase( station, stationid, db, h3, altitude, agl,
 	
 }
 
+//
+// Dump all of the output files that need to be dumped, this goes through
+// everything that may need to be written and writes it to the disk
+async function produceOutputFiles() {
+	console.log( `producing output files for ${stationDbCache.length} stations + global ` )
+	
+	// each of the stations
+	stationDbCache.forEach( async function (db,key) {
+		await produceOutputFile( key, db );
+	});
+		
+	// And the global output
+	await produceStationFile( statusDb );
+	await produceOutputFile( 'global', globalDb );
+
+	// Flush old database from the cache
+	stationDbCache.prune();
+}
+
 
 //
-// Produce the output 
+// Produce the output based on the accumulations in the database
+//
+// This outputs using Apache Arrow columnar table format which can be loaded
+// in a webworking by deck.gl speeding up the whole process.
+//
+// We generate a static file on a timed basis making it trivial for
+// distributing the load amongst either many servers or a CDN cache
+// it also has the advantage that you can keep a snapshot in time simply
+// by keeping the file.
 async function produceOutputFile( station, inputdb ) {
 	let length = 500;
 	let position = 0;
@@ -620,18 +649,16 @@ async function produceOutputFile( station, inputdb ) {
 	return position;
 }
 
-// Dump the meta data for all the stations, we take from DB 
+//
+// Dump the meta data for all the stations, we take from our in memory copy
+// it will have been primed on start from the db and then we update and flush
+// back to the disk
 async function produceStationFile( statusdb ) {
 
+	// Form a list of hashes
 	let statusOutput = _map( stations, (v,k) => {return { station: k, ...v };});
-	
-	// Go through all the keys
-//	for  const [key,value] of statusdb.iterator()) {
-//		if( ! ignoreStation(key) ) {
-//			statusOutput.push( {station:''+key, ...JSON.parse(''+value)})
-//		}
-//	}
 
+	// Write this to the stations.json file
 	writeFile( outputPath + 'stations.json', JSON.stringify(statusOutput,null,2), (err) => err ? console.log("station write error",err) : null);
 }
 	
