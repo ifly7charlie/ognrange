7a7d00bc4d70af20f256da14d6cfc0099b481760
ifly7charlie <melissa-github@littlebluecar.co.uk>
2022-10-05 21:06:00 +0200

    Output uncompressed arrow files

--- bin/dumparrow.js
+++ bin/dumparrow.js
@@ -1,46 +1,36 @@
+import {readFileSync} from 'fs';
+import {tableFromIPC, RecordBatchReader} from 'apache-arrow/Arrow.node';
 
-import { readFileSync } from 'fs';
-import { tableFromIPC } from 'apache-arrow/Arrow.node';
-
-
-import { DB_PATH, OUTPUT_PATH } from '../lib/bin/config.js';
+import {DB_PATH, OUTPUT_PATH} from '../lib/bin/config.js';
 import yargs from 'yargs';
 
+const args = yargs(process.argv.slice(2)) //
+    .option('station', {alias: 's', type: 'string', default: 'global', description: 'Arrow file'})
+    .option('file', {alias: 'f', type: 'string', default: 'year.arrow', description: 'Arrow file'})
+    .help()
+    .alias('help', 'h').argv;
 
-const args = yargs(process.argv.slice(2))
-	.option( 'station', 
-			 { alias: 's',
-			   type: 'string',
-			   default: 'global',
-			   description: 'Arrow file'
-		})
-	.option( 'file', 
-			 { alias: 'f',
-			   type: 'string',
-			   default: 'year.arrow',
-			   description: 'Arrow file'
-		})
-		.help()
-		.alias( 'help', 'h' ).argv;
+console.log(OUTPUT_PATH, {...args});
+import {open} from 'node:fs/promises';
+const fd = await open(OUTPUT_PATH + args.station + '/' + args.station + '.' + args.file);
 
+const reader = await RecordBatchReader.from(fd.createReadStream());
 
+for await (const batch of reader) {
+    console.log(batch.data.type);
+    let c = 0;
+    let d = 0;
 
-const arrow = readFileSync( OUTPUT_PATH + args.station + '/' + args.station + '.' +args.file );
-const table = tableFromIPC([arrow]);
+    let lo = 0;
+    let hi = 0;
+    for (const columns of batch) {
+        //	console.log(
+        let out = '';
+        const json = columns.toJSON();
 
-for( const columns of table ) {
-//	console.log(
-	let out = '';
-	for( const column of columns ) {
-		if( column[0] == 'h3' ) {
-			out = (BigInt(column[1])).toString(16) + ' {';
-		}
-		else {
-			out += `{"${column[0]}":${column[1]}},`;
-		}
-	}
-	console.log( out.slice(0,-1) + '}' );
+        json.h3 = json.h3hi.toString(16) + json.h3lo.toString(16);
+        delete json.h3lo;
+        delete json.h3hi;
+        console.log(JSON.stringify(json));
+    }
 }
-
-//console.table([...table].slice(0,100));
-
