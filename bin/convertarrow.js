127fc99601e6caeb34172680dfe3eadf05c3f48d
ifly7charlie <melissa-github@littlebluecar.co.uk>
2022-10-05 21:05:51 +0200

    Convert all arrow files in directory

--- bin/convertarrow.js
+++ bin/convertarrow.js
@@ -1,4 +1,4 @@
-import {readFileSync} from 'fs';
+import {existsSync, readFileSync, mkdirSync, readdirSync} from 'fs';
 import {tableFromIPC, RecordBatchReader} from 'apache-arrow/Arrow.node';
 
 import {Utf8, Uint8, Uint16, Uint32, Uint64, makeBuilder, makeTable, RecordBatchWriter} from 'apache-arrow/Arrow.node';
@@ -8,80 +8,99 @@ import {PassThrough} from 'stream';
 
 import zlib from 'zlib';
 
-import {DB_PATH, OUTPUT_PATH} from '../lib/bin/config.js';
+import {DB_PATH, OUTPUT_PATH, UNCOMPRESSED_ARROW_FILES} from '../lib/bin/config.js';
 import yargs from 'yargs';
 
 const NEW_PATH = OUTPUT_PATH + '../converted/';
 
-const args = yargs(process.argv.slice(2)) //
-    .option('station', {alias: 's', type: 'string', default: 'global', description: 'Arrow file'})
-    .option('file', {alias: 'f', type: 'string', default: 'year.arrow', description: 'Arrow file'})
-    .help()
-    .alias('help', 'h').argv;
+// Top level
+readdirSync(OUTPUT_PATH).forEach(function (subdir) {
+    // One dir for each station
+    console.log(subdir + ':');
+    try {
+        readdirSync(OUTPUT_PATH + subdir).forEach(function (file) {
+            let fileName = subdir + '/' + file;
+            if (fileName.match(/[0-9].arrow$/)) {
+                try {
+                    processFile(subdir, fileName);
+                } catch (e) {
+                    console.log('\t*** failed, ', String(e).split('\n')?.[0]);
+                }
+            }
+        });
+    } catch (e) {}
+});
 
-const arrowFile = readFileSync(OUTPUT_PATH + args.station + '/' + args.station + '.' + args.file);
-const table = tableFromIPC([arrowFile]);
+function processFile(station, fileName) {
+    const outputFileName = NEW_PATH + fileName;
+    const arrowFile = readFileSync(OUTPUT_PATH + fileName);
 
-console.log(table.schema.fields);
+    mkdirSync(NEW_PATH + station, {recursive: true});
 
-const h3lo = makeBuilder({type: new Uint32()});
-const h3hi = makeBuilder({type: new Uint32()});
-const numStations = makeBuilder({type: new Uint8()});
-
-// Accumulate the h3s into two structures
-console.log('H3...');
-let rowCount = 0;
-for (const h3part of table.getChild('h3')) {
-    if (rowCount % 2 == 0) {
-        h3lo.append(h3part);
-    } else {
-        h3hi.append(h3part);
+    if (existsSync(outputFileName)) {
+        console.log('\t' + fileName + '->' + outputFileName + ' * skipping as it exists');
+        return;
     }
-    rowCount++;
-}
 
-let globalType = !!table.getChild('stations');
+    const table = tableFromIPC([arrowFile]);
 
-if (globalType) {
-    console.log('Station Count...');
-    for (const station of table.getChild('stations')) {
-        numStations.append(Math.min(station?.split(',')?.length, 255));
+    if (!table.getChild('h3')) {
+        return;
     }
-}
 
-console.log('Generating updates');
-let arrow = {
-    h3lo: h3lo.finish().toVector(),
-    h3hi: h3hi.finish().toVector()
-};
-if (globalType) {
-    arrow.numStations = numStations.finish().toVector();
-}
-const tableUpdates = makeTable(arrow);
+    const h3lo = makeBuilder({type: new Uint32()});
+    const h3hi = makeBuilder({type: new Uint32()});
+    const numStations = makeBuilder({type: new Uint8()});
 
-console.log('Merging');
-{
-    const resultant = table //
-        .select(['minAgl', 'minAlt', 'minAltSig', 'maxSig', 'avgSig', 'avgCrc', 'count', 'avgGap', 'stations', 'expectedGap'])
-        .assign(tableUpdates);
+    // Accumulate the h3s into two structures
+    let rowCount = 0;
+    for (const h3part of table.getChild('h3')) {
+        if (rowCount % 2 == 0) {
+            h3lo.append(h3part);
+        } else {
+            h3hi.append(h3part);
+        }
+        rowCount++;
+    }
 
-    const fileName = NEW_PATH + args.station + '/' + args.station + '.' + args.file;
+    let globalType = !!table.getChild('stations');
 
+    if (globalType) {
+        for (const station of table.getChild('stations')) {
+            numStations.append(Math.min(station?.split(',')?.length, 255));
+        }
+    }
+
+    let arrow = {
+        h3lo: h3lo.finish().toVector(),
+        h3hi: h3hi.finish().toVector()
+    };
+    if (globalType) {
+        arrow.numStations = numStations.finish().toVector();
+    }
+    const tableUpdates = makeTable(arrow);
     {
-        const pt = new PassThrough({objectMode: true});
-        const result = pt //
-            .pipe(RecordBatchWriter.throughNode())
-            .pipe(createWriteStream(fileName));
-        pt.write(resultant);
-        pt.end();
+        const resultant = table //
+            .select(['minAgl', 'minAlt', 'minAltSig', 'maxSig', 'avgSig', 'avgCrc', 'count', 'avgGap', 'stations', 'expectedGap'])
+            .assign(tableUpdates);
+
+        if (UNCOMPRESSED_ARROW_FILES) {
+            const pt = new PassThrough({objectMode: true});
+            const result = pt //
+                .pipe(RecordBatchWriter.throughNode())
+                .pipe(createWriteStream(outputFileName));
+            pt.write(resultant);
+            pt.end();
+        }
+        {
+            const pt = new PassThrough({objectMode: true});
+            const result = pt
+                .pipe(RecordBatchWriter.throughNode())
+                .pipe(zlib.createGzip())
+                .pipe(createWriteStream(outputFileName + '.gz'));
+            pt.write(resultant);
+            pt.end();
+        }
     }
-    {
-        const pt = new PassThrough({objectMode: true});
-        const result = pt
-            .pipe(RecordBatchWriter.throughNode())
-            .pipe(zlib.createGzip())
-            .pipe(createWriteStream(fileName + '.gz'));
-        pt.write(resultant);
-        pt.end();
-    }
+    console.log('\t' + fileName + '->' + outputFileName + ',bytes:' + arrowFile.length);
 }
