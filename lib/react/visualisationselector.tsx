6819ac6c45f6624c6703cd06027453792ff1895d
ifly7charlie <melissa-github@littlebluecar.co.uk>
2026-03-12 09:32:15 +0100

    fix visualisation change bug

--- lib/react/visualisationselector.tsx
+++ lib/react/visualisationselector.tsx
@@ -73,7 +73,7 @@ export function VisualisationSelector({
         }
     }, [multiLayer]);
 
-    const selectVisualisationOnChange = useCallback((v) => setVisualisation(v.value), [false]);
+    const selectVisualisationOnChange = useCallback((v) => setVisualisation(v.value), [setVisualisation]);
 
     return (
         <>
